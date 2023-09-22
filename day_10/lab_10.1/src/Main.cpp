a6101aba8f20f566654e84a52a73551deb208098
73728aniket <117276936+Anikepardeshi@users.noreply.github.com>
2023-09-22 22:21:38 +0530

    Add files via upload

---
+++ day_10/lab_10.1/src/Main.cpp
@@ -0,0 +1,29 @@
+
+//instance counter
+
+
+#include<iostream>
+ using namespace std;
+ class InstanceCounter{
+ private:
+ static int count;
+ public:
+ InstanceCounter( ){
+ InstanceCounter::count = InstanceCounter::count + 1;
+ }
+ static int getCount( void ){
+ return InstanceCounter::count;
+ }
+ ~InstanceCounter( ){
+ InstanceCounter::count = InstanceCounter::count - 1;
+ }
+ };
+ int InstanceCounter::count = 0;
+ int main( void ){
+ InstanceCounter c1, c2, c3;
+ cout << "Instance Counter : " << InstanceCounter::getCount( ) <<
+endl;
+
+ return 0;
+
+ }
